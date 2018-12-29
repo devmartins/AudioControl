820a9d1924a94f2fcb26582532d31cc7b77696c7
Ildyone Martins <ildyone@gmail.com>
2018-12-29 19:58:37 -0200

    Commit inicial

---
+++ AudioControl/AudioControl/AudioControl.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System.Windowws.Media
+{
+    public static class AudioControl
+    {
+        [DllImport("AudioControlNative.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern float GetMasterVolume();
+
+        [DllImport("AudioControlNative.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern void SetMasterVolume(float volume);
+    }
+}
